3a205a84d8e56c6dfb43e422c2275a75132d4c0a
Stanley Zheng <dev.stanzheng@gmail.com>
2018-01-11 02:54:12 -0500

    remove small bug

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -3,7 +3,7 @@ class CheckinsController < ApplicationController
   def create
     # render(json:current_user)
     @checkin = Checkin.new(thing_id:params[:id])
-    @checkin.user_id = @current_user.id
+    @checkin.user_id = current_user.id
     @checkin.save!
     render(json:@checkin)
   end
