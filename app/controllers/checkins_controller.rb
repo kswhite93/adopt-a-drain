0efa77ec7cfa1b9fec3ca8cf49d91035e6e3b6e7
Stanley Zheng <dev.stanzheng@gmail.com>
2018-01-11 02:45:47 -0500

    adding action to controller

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,2 +1,15 @@
 class CheckinsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
+  def create
+    # render(json:current_user)
+    @checkin = Checkin.new(thing_id:params[:id])
+    @checkin.user_id = @current_user.id
+    @checkin.save!
+    render(json:@checkin)
+  end
+
+  def show
+    @checkin = Checkin.take()
+    render(json: @checkin)
+  end
 end
