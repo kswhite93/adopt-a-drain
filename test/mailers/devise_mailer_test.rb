c124bcde21bb33da8e07f0fd5c2be52d9920978e
Kevin Curry <kmcurry@gmail.com>
2018-01-24 19:31:10 -0500

    replace instances of SFWater

--- test/mailers/devise_mailer_test.rb
+++ test/mailers/devise_mailer_test.rb
@@ -7,7 +7,7 @@ class DeviseMailerTest < ActionMailer::TestCase
     email = DeviseMailer.reset_password_instructions(@user, '1234').deliver_now
 
     assert_not ActionMailer::Base.deliveries.empty?
-    assert_equal ['no-reply@sfwater.org'], email.from
+    assert_equal ['adoptstormdrain@norfolk.gov'], email.from
     assert_equal ['erik@example.com'], email.to
     assert_equal 'Adopt-a-drain Norfolk reset password instructions', email.subject
   end
